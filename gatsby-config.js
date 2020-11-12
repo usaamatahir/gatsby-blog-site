b91e0fd84d5e98b9b16813dce0134ef8b93afe27
usaamatahir <usamatahir8330@gmail.com>
2020-11-12 18:47:03 +0500

    REMOVE UNNECCESARY CODE

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,21 +20,9 @@ module.exports = {
         accessToken: process.env.CONTENT_FUL_ACCESS_TOKEN,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `@contentful/gatsby-transformer-contentful-richtext`,
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
 
     {
       resolve: "gatsby-plugin-firebase",
